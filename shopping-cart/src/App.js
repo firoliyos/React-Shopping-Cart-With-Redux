5a513f9ad3af3755aeb63c8527452b7217fdd925
firoliyos <firatesfaye44@gmail.com>
2024-11-27 21:41:11 +0300

    Header-changed

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Cart from './pages/cart';
+import Header from './components/header';
 function App() {
   return (
     <div>
-      <h1>NAVBAR</h1>  
+      <Header/> 
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/cart' element={<Cart/>}/>

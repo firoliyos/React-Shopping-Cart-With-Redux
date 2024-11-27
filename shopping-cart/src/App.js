a39dcb0898dc3a45a0dd2ceae9d13ee590b8c21a
firoliyos <firatesfaye44@gmail.com>
2024-11-27 22:27:19 +0300

    done

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Home from './pages/home';
-import Cart from './pages/cart';
-import Header from './components/header';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Cart from "./pages/cart";
+import Header from "./components/header";
+
 function App() {
   return (
     <div>
-      <Header/> 
+      <Header/>
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/cart' element={<Cart/>}/>
-      </Routes>   
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
